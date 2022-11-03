13cfbb29741b460bed7cd122cb12120c918a865b
unknown <cyprian.dankowski@gmail.com>
2022-11-03 19:38:13 +0100

    added cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import userRouter from './routes/user.js';
 import session from 'express-session';
 import cookieParser from "cookie-parser";
+import cors from 'cors';
 
 const PORT = process.env.BACKEND_PORT;
 const MONGO_DB_URL = `mongodb://${process.env.MONGODB_ADMIN_USERNAME}:${process.env.MONGODB_ADMIN_PASSWORD}@mongo:${process.env.MONGODB_PORT}/${process.env.MONGODB_DATABASE_NAME}?authSource=admin`
@@ -22,7 +23,12 @@ app.use(session({
     saveUninitialized: true,
     name: 'sesID'
 }));
-
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH'],
+    exposedHeaders: ['sesID'],
+    credentials: true
+}));
 
 app.use('/user', userRouter);
 
