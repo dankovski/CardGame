f851927f0238c682677b70feded0cd5b6c9206f2
unknown <cyprian.dankowski@gmail.com>
2022-11-03 19:23:05 +0100

    init

---
+++ backend/src/routes/user.ts
@@ -0,0 +1,93 @@
+import crypto from 'crypto'
+import express from 'express';
+import User from '../models/user.js';
+import bcrypt from 'bcrypt';
+
+const router = new express.Router();
+
+router.post('/register', async (req, res) => {
+    if (!req.body.email || !req.body.username || !req.body.password) {
+        return res.status(400).json({ message: 'Missing fields' });
+    }
+
+    const user = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        activationCode: crypto.randomUUID()
+    });
+
+    try {
+        await user.save();
+        res.status(201).json({ success: true });
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+
+});
+
+router.get('/activate', async (req, res) => {
+    if (!req.query.username || !req.query.activationCode) {
+        return res.status(400).json({ message: 'missing fields' });
+    }
+
+    const user = await User.findOne({ username: req.query.username });
+    if (user && user.status !== 'Pending') {
+        return res.status(410).json({ message: 'account already activated' });
+    }
+    else if (user && user.activationCode === req.query.activationCode) {
+        user.status = 'Active';
+        await user.save();
+        return res.status(200).json({ message: 'account has been activated' });
+    }
+    else {
+        return res.status(400).json({ message: 'incorrect data' });
+    }
+
+});
+
+router.post('/login', async function (req, res) {
+    const loggedUser = await User.findOne({ username: req.session.username });
+    if (loggedUser) {
+        return res.status(200).json({
+            username: loggedUser.username,
+            email: loggedUser.email,
+            avatarUrl: loggedUser.avatarUrl,
+            dateCreated: loggedUser.dateCreated,
+            dateUpdated: loggedUser.dateUpdated
+        });
+    } else if (!req.body.username || !req.body.password) {
+        return res.status(400).json({ message: 'Missing fields' });
+    } else {
+        const user = await User.findOne({ username: req.body.username });
+        if (user && await bcrypt.compare(req.body.password, user.password)) {
+            req.session.username = user.username
+            return res.status(200).json({ message: 'Logged in correctly' });
+        } else {
+            return res.status(400).json({ message: 'Unable to login' });
+        }
+    }
+});
+
+router.get('/me', async function (req, res) {
+    const user = await User.findOne({ username: req.session.username });
+    if (user) {
+        return res.status(200).json({
+            username: user.username,
+            email: user.email,
+            avatarUrl: user.avatarUrl,
+            dateCreated: user.dateCreated,
+            dateUpdated: user.dateUpdated
+        });
+    }
+    else {
+        return res.status(401).json({ message: 'Unable to authorize' });
+    }
+});
+
+router.get('/logout', async function (req, res) {
+    req.session.destroy()
+    return res.status(200).json({ message: 'Logged out' });
+});
+
+export default router;
---
+++ backend/src/server.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import userRouter from './routes/user.js';
+import session from 'express-session';
+import cookieParser from "cookie-parser";
+
+const PORT = process.env.BACKEND_PORT;
+const MONGO_DB_URL = `mongodb://${process.env.MONGODB_ADMIN_USERNAME}:${process.env.MONGODB_ADMIN_PASSWORD}@mongo:${process.env.MONGODB_PORT}/${process.env.MONGODB_DATABASE_NAME}?authSource=admin`
+
+await mongoose.connect(MONGO_DB_URL, { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const app=express();
+
+app.use(express.json())
+app.use(cookieParser());
+app.use(session({
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: true,
+    name: 'sesID'
+}));
+
+
+app.use('/user', userRouter);
+
+app.listen(PORT, '0.0.0.0');
