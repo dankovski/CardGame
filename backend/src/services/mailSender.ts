7ed0e008bc408ee6c33f8d8b7f2986e64b02ab54
unknown <cyprian.dankowski@gmail.com>
2022-11-29 13:35:09 +0100

    refactor(backend): code refactor and eslint fix

--- backend/src/services/mailSender.ts
+++ backend/src/services/mailSender.ts
@@ -3,34 +3,34 @@ import handlebars from 'handlebars';
 import fs from 'fs';
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.MAIL_LOGIN,
-      pass: process.env.MAIL_PASSWORD
-    }
-  });
+	service: 'gmail',
+	auth: {
+		user: process.env.MAIL_LOGIN,
+		pass: process.env.MAIL_PASSWORD
+	}
+});
   
-  const source = fs.readFileSync('./src/templates/activationMail.html', 'utf-8').toString();
-  const template = handlebars.compile(source);
+const source = fs.readFileSync('./src/templates/activationMail.html', 'utf-8').toString();
+const template = handlebars.compile(source);
 
-  export async function sendActivationLink(userMail: String, activationCode: String, username: String) {
-    const replacements = {
-      activationLink: `http://${process.env.BACKEND_HOST}:${process.env.BACKEND_PORT}/user/activate?username=${username}&activationCode=${activationCode}`,
-      username: username
-    };
-    const htmlToSend = template(replacements);
-    const mailOptions = {
-      from: process.env.MAIL_LOGIN,
-      to: userMail,
-      subject: 'Welcome in Card Game!',
-      html: htmlToSend
-    };
-    transporter.sendMail(mailOptions, function(error, info){
-      if (error) {
-        console.log(error);
-      } else {
-        console.log('Email sent: ' + info.response);
-      }
-    });
+export async function sendActivationLink(userMail: string, activationCode: string, username: string) {
+	const replacements = {
+		activationLink: `http://${process.env.BACKEND_HOST}:${process.env.BACKEND_PORT}/user/activate?username=${username}&activationCode=${activationCode}`,
+		username: username
+	};
+	const htmlToSend = template(replacements);
+	const mailOptions = {
+		from: process.env.MAIL_LOGIN,
+		to: userMail,
+		subject: 'Welcome in Card Game!',
+		html: htmlToSend
+	};
+	transporter.sendMail(mailOptions, function(error, info) {
+		if (error) {
+			console.log(error);
+		} else {
+			console.log('Email sent: ' + info.response);
+		}
+	});
 
-  }
+}
